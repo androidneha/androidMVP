97ec6279ece9664d5e354bef9b5542a549d06148
Ashok kumar <ashok.kumar@ashoks-MacBook-Pro-2.local>
2017-07-22 23:42:09 +0530

    Initial setup with view class

---
+++ app/src/main/java/com/ashokslsk/mvpexample/view/SigninView.java
@@ -0,0 +1,13 @@
+package com.ashokslsk.mvpexample.view;
+
+/**
+ * Created by ashok.kumar on 22/07/17.
+ */
+
+public interface SigninView {
+
+    void showValidationError();
+    void signInSuccess();
+    void signInError();
+
+}
